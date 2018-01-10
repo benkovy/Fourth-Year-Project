7bf0a109f4db627cc7c557b1f56abaf7a57e31dd
benkovacs <kovacs1@live.ca>
2018-01-10 12:38:35 -0400

    Initial Commit

---
+++ FYPv4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FYPv4
+//
+//  Created by Ben Kovacs on 2018-01-10.
+//  Copyright © 2018 BenKovacs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
